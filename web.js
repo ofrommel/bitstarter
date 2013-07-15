1f665987028fa23b71aaffebff68ef6e477dab01
zetalab <contact@zetalab.de>
2013-07-16 00:44:54 +0200

    added bootstrap

--- web.js
+++ web.js
@@ -9,7 +9,27 @@ app.get('/', function(request, response) {
   response.send(data);
 });
 
-var port = process.env.PORT || 5000;
+app.get('/bootstrap/*', function(req, res){
+    path = req.params[0];
+    res.sendfile('./bootstrap/' + path);
+});
+
+/*app.get('/bootstrap.css', function(request, response) {
+  data = fs.readFileSync("bootstrap.css", "utf8");
+  response.send(data);
+});
+
+app.get('/bootstrap-responsive.css', function(request, response) {
+  data = fs.readFileSync("bootstrap-responsive.css", "utf8");
+  response.send(data);
+});
+
+app.get('/bootstrap.js', function(request, response) {
+  data = fs.readFileSync("bootstrap.js", "utf8");
+  response.send(data);
+});
+*/
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
